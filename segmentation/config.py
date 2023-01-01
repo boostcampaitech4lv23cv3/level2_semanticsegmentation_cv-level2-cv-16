8c207e44309261ad10c1d1d835b210216b5f53bf
FIN443 <jjhun1228@gmail.com>
2023-01-01 08:55:01 +0000

    albu

--- segmentation/config.py
+++ segmentation/config.py
@@ -1,10 +1,10 @@
 class Config:
     config='./configs/_custom_/mask2former_beitv2_adapter_large_896_80k_cocostuff164k_ss.py'
-    exp_name='beitv2_adapter_large_data_v2_fold0'
+    exp_name='beitv2_adapter_large_data_v1_fold0_albu_1'
     work_dir=f'./work_dirs/{exp_name}'
     wandb_project='Trash_Seg'
     wandb_entity='jjhun1228'
-    load_from='weights/mask2former_beitv2_adapter_large_896_80k_cocostuff164k.pth'
+    load_from='./weights/mask2former_beitv2_adapter_large_896_80k_cocostuff164k.pth'
     resume_from=None
     no_validate=None
     gpus=None
--- segmentation/configs/_custom_/mask2former_beitv2_adapter_large_896_80k_cocostuff164k_ss.py
+++ segmentation/configs/_custom_/mask2former_beitv2_adapter_large_896_80k_cocostuff164k_ss.py
@@ -150,4 +150,4 @@ data = dict(samples_per_gpu=1,
             test=dict(pipeline=test_pipeline))
 runner = dict(type='IterBasedRunner')
 checkpoint_config = dict(by_epoch=False, interval=1000, max_keep_ckpts=1)
-evaluation = dict(start=40000, interval=2000, metric='mIoU', save_best='mIoU')
+evaluation = dict(start=30000, interval=2000, metric='mIoU', save_best='mIoU')
